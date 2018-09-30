e7a95b85f89749c5a7db20382705e46fb664233f
decode18 <dd.wahyu16@gmail.com>
2018-09-30 20:07:30 +0700

    tes variable

--- main.go
+++ main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("test bos")
+	var firstName string = "john"
+
+	//var lastName string
+	lastName := "wickss"
+
+	fmt.Printf("hallo %s %s!\n", firstName, lastName)
+	fmt.Println("haloo", firstName, lastName + "!")
+
+	name :=  new(string)
+
+	fmt.Println(name)
+	fmt.Println(*name)
 }
