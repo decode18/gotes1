3946e8fbc612a4014c67806bad9438e3253cc2e6
decode18 <dd.wahyu16@gmail.com>
2019-11-18 23:23:02 +0700

    jenkin tes

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello buos jenkins, %s!", r.URL.Path[1:])
+    fmt.Fprintf(w, "Hello buos jenkins hehe, %s!", r.URL.Path[1:])
 }
