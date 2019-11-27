d1544f3aa87ae5cc89b6cc5b18aa6751de407c23
decode18 <dd.wahyu16@gmail.com>
2019-11-27 20:44:55 +0700

    Update main.go

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello buos jenkins hehe sedooot, %s!", r.URL.Path[1:])
+    fmt.Fprintf(w, "Test Jenkins, %s!", r.URL.Path[1:])
 }
